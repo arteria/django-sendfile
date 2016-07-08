36259c9ef8a0aec43057c07c116cbd4dbba79c72
Philippe O. Wagner <wagner@arteria.ch>
2016-07-08 15:45:43 +0200

    bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.command.build_py import build_py
 
 
-version = __import__('sendfile').__version__
+version = __import__('sendfile').__version__ + ".post1"
 
 
 setup(
